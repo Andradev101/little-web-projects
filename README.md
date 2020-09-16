76d7d2c024e8b945d577d8574a9e6b6a9cc033c9
Luiz Henrique Santos de Andrade <71150787+PegasusAndrade@users.noreply.github.com>
2020-09-16 17:37:18 -0300

    Added changecase

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Web
- Web little projects
+ First repository!
+ This is my little web projects repository, it contains mainly the cool stuff i find while surfing the web, making and enhancing them whenever i can.
+ I'll also provide links for everything possible.
+
---
+++ changecasev0.2/main.js
@@ -0,0 +1,47 @@
+    const upbtn = document.getElementById("upbtn");
+    const downbtn = document.getElementById("downbtn");
+    const capbtn = document.getElementById("capbtn");
+    const phrabtn = document.getElementById("phrabtn");
+    const textcopy = document.getElementById("textcopy");
+
+    
+    upbtn.addEventListener("click", Upper);
+    downbtn.addEventListener("click", Lower);
+    capbtn.addEventListener("click", Capitalize);
+    phrabtn.addEventListener("click", Phrasecaps);
+
+    //main
+    function getText(){
+        msg = document.getElementById('txtarea').value; 	//atribui texto digitado a msg
+    }
+    
+    //btns functions
+    function Upper(){
+        getText();
+        var result = msg.toUpperCase();						//transforma em uppercase
+        document.getElementById("txtarea").value=result;	//printa de volta no txtarea
+    }
+
+    function Lower(){
+        getText();
+        var result = msg.toLowerCase();						//transforma em uppercase
+        document.getElementById("txtarea").value=result;	//printa de volta no txtarea	
+    }
+
+    function Capitalize(){					
+        getText();
+        var capString = (msg) => msg[0].toUpperCase() + msg.slice(1).toLowerCase();
+        msg = msg.replace(/\r\n|\n|\r/gm, "");
+        var words = msg.split(' ').map(capString);
+        var result = words.join(' ');
+        document.getElementById('txtarea').value = result;
+    }		
+
+    function Phrasecaps(){					
+        getText();
+        var capString = (msg) => msg[0].toUpperCase() + msg.slice(1).toLowerCase();
+        msg = msg.replace(/\r\n|\n|\r/gm, "");
+        var phrase = msg.split('. ').map(capString);
+        var result = phrase.join('. ');
+        document.getElementById("txtarea").value = result; //troca o msg do txtarea para o de result			
+    }
