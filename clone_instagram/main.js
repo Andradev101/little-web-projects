6516751b29f4cef91bdf78c063116dba50fb7fa1
Luiz Henrique Santos de Andrade <71150787+PegasusAndrade@users.noreply.github.com>
2020-09-22 23:46:03 -0300

    Added main.js

---
+++ clone_instagram/main.js
@@ -0,0 +1,7 @@
+const image = document.getElementById("image");
+let images = ["img/showcase_1.jpg","img/showcase_2.jpg","img/showcase_3.jpg"], i = 0;
+function changephoto() {
+    i<images.length-1?i+=1:i=0; 
+    image.src=images[i];
+}
+setInterval(changephoto, 3000);
